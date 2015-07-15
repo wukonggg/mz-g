4cc305b6c2774a9651ee4153519f8abdd87ca310
wukong <wukonggg@139.com>
2015-07-15 14:36:42 +0800

    更新一下常用select语句
    更新一下常用select语句

--- sql/mz-g-select.sql
+++ sql/mz-g-select.sql
@@ -8,8 +8,17 @@ inner join t_sku_clothing_more t2 on t2.sku_clothing_id = t.id
 where t.state = 1 and t1.state = 1
 and t.id = 2;
 
+-- 查询一个skumore的详细信息
+select t2.gname, t2.cate_code,
+       t1.sid, t1.model, t1.`type`, t1.pprice, t1.sprice, t1.state,
+       t.size, t.count
+from t_sku_more t
+inner join t_sku t1 on t1.id = t.sku_id
+inner join t_goods t2 on t2.id = t1.goods_id
+where t.id = 140;
 
-SELECT t.cate_code, t.item, t.name, t.title, t.evalue, t.cvalue, t.name_order, t.value_order
-FROM t_sku_dict t
-WHERE t.cate_code='91000' AND t.item='SIZE_STANDARD' AND t.name='DEFAULT'
-ORDER BY t.name_order;
+-- 查询sku_prop_type模板
+select t.cate_code, t.item, t.name, t.title, t.evalue, t.cvalue, t.name_order, t.value_order
+from t_sku_prop_type t
+where t.cate_code='S-1' and t.item='SIZE_STANDARD' and t.name='SIZE'
+order by t.name_order;
