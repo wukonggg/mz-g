d9fad149fbfdd3f836e2900de011042e313c3d57
wukong <wukonggg@139.com>
2015-09-05 17:14:02 +0800

    完善Druid监控
    完善Druid监控

---
+++ src/main/java/band/wukong/mz/nutz/NutDruidFilter.java
@@ -0,0 +1,40 @@
+package band.wukong.mz.nutz;
+
+import org.nutz.mvc.NutFilter;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * As you see...
+ *
+ * @author wukong(wukonggg@139.com)
+ */
+public class NutDruidFilter extends NutFilter {
+
+    protected Set<String> prefixs = new HashSet<String>();
+
+
+    public void init(FilterConfig conf) throws ServletException {
+        super.init(conf);
+        prefixs.add(conf.getServletContext().getContextPath() + "/druid/");
+        prefixs.add(conf.getServletContext().getContextPath() + "/rs/");
+    }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException, ServletException {
+        if (req instanceof HttpServletRequest) {
+            String uri = ((HttpServletRequest) req).getRequestURI();
+            for (String prefix : prefixs) {
+                if (uri.startsWith(prefix)) {
+                    chain.doFilter(req, resp);
+                    return;
+                }
+            }
+        }
+        super.doFilter(req, resp, chain);
+    }
+}
--- src/main/resources/ioc/db.js
+++ src/main/resources/ioc/db.js
@@ -11,19 +11,24 @@ var ioc = {
             depose: 'close'
         },
         fields: {
-            driverClassName : {java :"$config.get('db-driver')"},
-            url             : {java :"$config.get('db-url')"},
-            username        : {java :"$config.get('db-username')"},
-            password        : {java :"$config.get('db-password')"},
-            initialSize: 1,
-            minIdle: 1,
-            maxActive: 2,
-            maxWait: 60000,
-            timeBetweenEvictionRunsMillis: 60000,
-            minEvictableIdleTimeMillis: 300000,
-            poolPreparedStatements: true,
-            maxPoolPreparedStatementPerConnectionSize: 20,
-            testWhileIdle:true
+            driverClassName      : {java : "$config.get('db.driver')"},
+            url                  : {java : "$config.get('db.url')"},
+            username             : {java : "$config.get('db.username')"},
+            password             : {java : "$config.get('db.password')"},
+            testWhileIdle        : {java : "$config.get('db.testWhileIdle')"},
+            validationQuery      : {java : "$config.get('db.validationQuery')"},
+            maxActive            : {java : "$config.get('db.maxActive')"},
+            filters              : {java : "$config.get('db.maxActive')"},
+            connectionProperties : {java : "$config.get('db.connectionProperties')"}
+            /*
+             initialSize: 1,
+             minIdle: 1,
+             maxWait: 60000,
+             timeBetweenEvictionRunsMillis: 60000,
+             minEvictableIdleTimeMillis: 300000,
+             poolPreparedStatements: true,
+             maxPoolPreparedStatementPerConnectionSize: 20
+             */
         }
     },
     dao: {
