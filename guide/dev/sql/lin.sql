e942da9e5bd5ca2f1475a875de07d7c7028e6113
wukong <wukonggg@139.com>
2016-09-10 14:27:56 +0800

    调调调

--- guide/dev/sql/lin.sql
+++ guide/dev/sql/lin.sql
@@ -3,3 +3,6 @@
 -- add：goods下架功能。sku的值为0时自动下架。退货时如果库存为0，需要自动上架。
 ALTER TABLE `t_goods` CHANGE COLUMN `state` `state` CHAR(2) NOT NULL COMMENT '0-已删除, 1-有效' AFTER `utime`;
 ALTER TABLE `t_sku` CHANGE COLUMN `state` `state` CHAR(2) NOT NULL COMMENT '0-已删除, 1-上架, 2-下架' AFTER `utime`;
+
+-- [now]调整打折策略(db-0.6.3)。支付时不再计算customer的clothingPayment，去除customer表的clothingPayment。
+ALTER TABLE `t_customer` DROP COLUMN `payment_clothing`;
